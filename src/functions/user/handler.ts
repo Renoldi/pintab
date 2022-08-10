73a531916bd7de9116294186bc506d0caa9ec85b
renoldiboz <renoldiboz@gmail.com>
2022-08-10 15:40:41 +0700

    change

--- src/functions/user/handler.ts
+++ src/functions/user/handler.ts
@@ -6,7 +6,15 @@ import usersService from '../../service'
 import { v4 } from "uuid";
 import { hashPass, verify } from "../../service/bcryptjs"
 import { generateToken } from "src/service/jwt";
-// import { generateToken } from "src/service/jwt";
+
+import * as yup from "yup";
+import User from "src/model/User";
+
+const schema = yup.object().shape({
+    name: yup.string().required(),
+    password: yup.string().required(),
+});
+
 
 export const getAllUsers = middyfy(async (): Promise<APIGatewayProxyResult> => {
     const users = await usersService.getAllUsers();
@@ -17,11 +25,15 @@ export const getAllUsers = middyfy(async (): Promise<APIGatewayProxyResult> => {
 
 export const createUser = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+
+        const reqBody:User = JSON.parse(JSON.stringify(event.body))
+        await schema.validate(reqBody, { abortEarly: false });
+
         const id = v4();
-        const passwordh = await hashPass(event.body.password)
+        const passwordh = await hashPass(reqBody.password)
         const user = await usersService.createUser({
+            ...reqBody,
             id: id,
-            name: event.body.name,
             password: passwordh,
             createdAt: new Date().toISOString(),
         })
@@ -54,13 +66,16 @@ export const getUser = middyfy(async (event: APIGatewayProxyEvent): Promise<APIG
 export const updateUser = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const id = event.pathParameters.id;
     try {
+        const reqBody:User = JSON.parse(JSON.stringify(event.body))
+        await schema.validate(reqBody, { abortEarly: false });
+        const passwordh = await hashPass(reqBody.password)
         const user = await usersService.updateUser(id, {
-            name: event.body.name,
-            password: event.body.password,
+            ...reqBody,
+            password: passwordh,
             updatedAt: new Date().toISOString(),
         })
         return formatJSONResponse({
-            user, id
+            user
         });
     } catch (e) {
         return formatJSONResponse({
@@ -73,12 +88,14 @@ export const updateUser = middyfy(async (event: APIGatewayProxyEvent): Promise<A
 export const loginUser = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
     try {
+        const reqBody:User = JSON.parse(JSON.stringify(event.body))
+        await schema.validate(reqBody, { abortEarly: false });
         const users = await usersService.loginUser(
-            event.body.name
+            reqBody.name
         )
 
         const user = users[0]
-        const veri = await verify(event.body.password, user.password)
+        const veri = await verify(reqBody.password, user.password)
         if (!veri) {
             return formatJSONResponse({
                 status: 500,
